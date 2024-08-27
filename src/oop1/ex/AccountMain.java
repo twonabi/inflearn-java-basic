00f5edac8a657fc04bf4265ef1e2892c0ea87335
jian <loveinansan1@gmail.com>
2024-08-27 15:03:38 +0900

    #23 김영한의 실전 자바 Basic
    class3_객체 지향 프로그래밍_EX2
    
    문제2) 객체 지향 계좌

---
+++ src/oop1/ex/AccountMain.java
@@ -0,0 +1,13 @@
+package oop1.ex;
+
+public class AccountMain {
+
+    public static void main(String[] args) {
+        Account account = new Account();
+        account.deposit(10000);
+        account.withdraw(9000);
+        account.withdraw(2000); // 오류 메세지 출력
+        System.out.println("잔고: " + account.balance);
+
+    }
+}
