ec860050ac5c6d9e05db35b3004deb33eff9a696
jian <loveinansan1@gmail.com>
2024-08-28 13:36:05 +0900

    #25 김영한의 실전 자바 Basic
    class4_생성자_this
    
    MemberInit- initMember() 추가
    
    initMember(..) 는 초기값 설정 기능을 제공하는 메서드 이다. 다음과 같이 메서드를 호출하면 객체의 맴버 변수에 인자로 넘어온 값을 채운다.
    member1.initMember("user1",15,90)
    
    **))
    MemberInit 의 initMember(String name,...) 코드를 보면, 메서드의 매개변수에 정의한 String name과 Member의 멤버 변수의 이름이 String name으로 둘다 똑같다. 나머지 변수 이름도 name, age, grade 로 모두 같다.
    
    그렇다면!
    멤버 변수와 메서드의 매개변수의 이름이 같으면 둘을 어떻게 구분해야 할까??
    - 이 경우 멤버 변수보다 매개변수가 코드 블럭의 더 안쪽에 있기 때문에 매개변수가 우선순의를 가진다. 따라서 initMember(String name,...) 메서드 안에서 name 이라고 적으면 매개변수에 접근하게 된다.
    - 멤버 변수에 접근하려면 앞에-- this. --이라고 해주면 된다.
    ** 여기서 this 는 인스턴스 자신의 참조값을 가리킨다.
    
    만약에 this. 을 제거하면 name은 둘다 매개변수를 뜻하게 된다. 따라서 맴버변수의 값이 변경되지 않는다
    
    정리)))))
    - 매개변수의 이름과 맴버 변수의 이름이 같은 경우 this를 사용해서 둘을 명확하게 구분해야 한다.
    - this 는 인스턴스 자신을 가리킨다.

--- src/construct/MemberInit.java
+++ src/construct/MemberInit.java
@@ -4,4 +4,14 @@ public class MemberInit {
     String name;
     int age;
     int grade;
+
+    //추가,  this 추가( * 이유 = 더 가까운 것에서 우선순위로 쓰기 때문에..
+    // 위에 있는 나의 인스턴스의 name, gae, grade를 쓰기 위해 this를 쓴다.
+    // initMember(..) 는 초기값 설정 기능을 제공하는 메서드 이다.
+    // 외부에서 자신의 값을 받아서 자신의 값을 바꾼다.
+    void initMember(String name, int age, int grade) { // (this에서 꺼내오지 않으면 이곳에서 꺼내서 사용)
+        this.name = name;
+        this.age = age;
+        this.grade = grade;
+    }
 }
