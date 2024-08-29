138c3b2d83285563651c5fc5ca2878d9b5c2e846
jian <loveinansan1@gmail.com>
2024-08-29 13:13:08 +0900

    #29 김영한의 실전 자바 Basic
    class4_생성자_오버로딩
    
    생성자도 메서드 오버로딩처럼 매개변수만 다르게 해서 여러 생성자를 제공할 수 있다.
    
    - 생성자를 오버로딩 한 덕분에 성적 입력이 꼭 필요한 경우에는 grade가 있는 생성자를 호출하면 되고, 그렇지 않은 경우애는 grade가 없는 생성자를 호출하면 된다. grade가 없는 생성자를 호출하면 성적은 50점이 된다.

--- src/construct/MemberConstruct.java
+++ src/construct/MemberConstruct.java
@@ -5,6 +5,14 @@ public class MemberConstruct {
     int age;
     int grade;
 
+    // 추가 (오버로딩)
+    MemberConstruct(String name, int age) {
+        this.name = name;
+        this.age = age;
+        this.grade = 50;
+
+    }
+
     MemberConstruct(String name, int age, int grade) {
         System.out.println("생성자 호출 name=" + name + ",age" + age + ",grade=" + grade);
         this.name = name;
