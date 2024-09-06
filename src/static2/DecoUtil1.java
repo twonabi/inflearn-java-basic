c2f546b2c9b835179232162d3f62976f13da9a28
jian <loveinansan1@gmail.com>
2024-09-06 12:30:43 +0900

    #47 김영한의 실전 자바 Basic
    class7_자바 메모리 구조와 static_메서드1
    
    DecoUtil2.deco(s) 코드를 보자.
    static 이 붙은 정적 메서드는 객체 생성 없이 클래스명 + .(dot) + 메서드 명으로 바로 호출할 수 있다.
    정적 메서드 덕분에 불필요한 객체 생성 없이 편리하게 메서드를 사용했다.
    
    클래스 메서드))
    메서드 앞에도 static 을 붙일 수 있다. 이것을 정적 메서드 또는 클래스 메서드라 한다. 정적 메서드라는 용어는 static이 정적이라는 뜻이기 때문이고, 클래스 메서드라는 용어는 인스천스 생성 없이 마치 클래스에 있는 메서드를 바로 호출하는 것 처럼 느껴지기 때문이다.
    
    인스턴스 메서드))
    static이 붙지 않는 메서드는 인스턴스를 생성해야 호출할 수 있다., 이것을 인스턴스 메서드라 한다.

---
+++ src/static2/DecoUtil1.java
@@ -0,0 +1,10 @@
+package static2;
+
+public class DecoUtil1 {
+
+    public String deco(String str) {
+       // return "*" + str + "*";
+        String result = "*" + str + "*";
+        return result;
+    }
+}
---
+++ src/static2/DecoUtil2.java
@@ -0,0 +1,10 @@
+package static2;
+
+public class DecoUtil2 {
+
+    public static String deco(String str) {
+       // return "*" + str + "*";
+        String result = "*" + str + "*";
+        return result;
+    }
+}
