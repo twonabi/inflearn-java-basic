4dd889bfa53e0ae00883885af9b165afc117eeb9
jian <loveinansan1@gmail.com>
2024-09-06 15:34:29 +0900

    #49 김영한의 실전 자바 Basic
    class7_자바 메모리 구조와 static_메서드3
    
    import를 사용하면 정적 메서드 자체를 사용하겠다는 의미로, 선언하는 것이어서 클래스 명을 사용하지 않아도 사용할수 있게 된다.
    DecoData.staticCall();
    -> import static static2.DecoData.staticCall;
    =  staticCall();
    
    용어정리))
    멤버 메서드의 종류
    - 인스턴스 메서드: static 이 붙지 않은 멤버 메서드
    - 클래스 메서드: static 이 붙은 멤버 메서드
    -- 클래스 메서드, 정적 메서드, static 메서드 등으로 부른다.
    
    static이 붙지 않은 멤버 메서드는 인스턴스를 생성해야 사용할 수 있고, 인스턴스에 소속되어 있다. 따라서 인스턴스 메서드라 한다. static이 붙은 멤버 메서드는 인스턴스와 무관하게 클래스에 바로 접근해서 사용할 수 있고, 클래스 자체에 소속되어 있다. 따라서 클래스 메서드라 한다.
    
    정적 메서드 활용))
    정적 메서드는 객체 생성이 필요 없이 메서드의 호출만으로 필요한 기능을 수행할 때 주로 사용한다.
    예를 들어 간단한 메서드 하나로 끝나는 유틸리티성 메서드레 자주 사용한다. 수학의 여러가지 기능을 담은 클래스를 만들 수 있는데, 이 경우 인스턴스 변수 없이 입력한 값을 계산하고 반환하는 것이 대부분인다. 이럴 때 정적 메서드를 사용해서 유틸리티성 메서드를 만들면 좋다.

--- src/static2/DecoDataMain.java
+++ src/static2/DecoDataMain.java
@@ -1,10 +1,14 @@
 package static2;
 
+//import static static2.DecoData.staticCall;
 public class DecoDataMain {
-
 
     public static void main(String[] args) {
         System.out.println("1. 정적 호촐");
+        DecoData.staticCall();
+        DecoData.staticCall();
+        DecoData.staticCall();
+        DecoData.staticCall();
         DecoData.staticCall();
 
         System.out.println("2. 인스턴스 호출1");
@@ -14,5 +18,14 @@ public class DecoDataMain {
         System.out.println("3. 인스턴스 호출2");
         DecoData data2 = new DecoData();
         data2.instanceCall();
+
+        //추가
+        // 인스턴스를 통한 접근 . // 권장 안함.
+        // 인텔리제이에서도 이렇게 하면 안된다고 한디. 클래스를 통한 접근을 해야 한다.
+        DecoData data3 = new DecoData();
+        data3.staticCall();
+
+        // 클래스를 통한 접근. 명확하게 이렇게 해야 코드를 이해하기 좋다.
+        DecoData.staticCall();
     }
 }
