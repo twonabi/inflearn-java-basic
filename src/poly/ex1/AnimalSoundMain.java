909ef5ae7f1d53ae4114fadb38c3deaa4308ad44
jian <loveinansan1@gmail.com>
2024-09-23 13:10:29 +0900

    #72 김영한의 실전 자바 Basic
    class11_다형성2_다형성 활용1

---
+++ src/poly/ex1/AnimalSoundMain.java
@@ -0,0 +1,22 @@
+package poly.ex1;
+
+public class AnimalSoundMain {
+
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        Cow cow = new Cow();
+        Cat cat = new Cat();
+
+        System.out.println("동물소리 테스트 시작");
+        dog.sound();
+        System.out.println("동물소리 테스트 종료");
+
+        System.out.println("동물소리 테스트 시작");
+        cow.sound();
+        System.out.println("동물소리 테스트 종료");
+
+        System.out.println("동물소리 테스트 시작");
+        cat.sound();
+        System.out.println("동물소리 테스트 종료");
+    }
+}
