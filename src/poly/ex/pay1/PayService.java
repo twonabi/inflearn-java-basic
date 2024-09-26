0e2d88103b2543b2aba5eef56bc411af2c30e734
jian <loveinansan1@gmail.com>
2024-09-26 17:50:37 +0900

    #86 김영한의 실전 자바 Basic
    class12_다형성3_ex2_2
    
    결제 시스템 개발- 리펙토링 1
    
    요구사항)
    - OCP 원칙을 지키세요.

---
+++ src/poly/ex/pay1/PayService.java
@@ -0,0 +1,33 @@
+package poly.ex.pay1;
+
+public class PayService {
+
+    public void processPay(String option, int amount) {
+
+        boolean result = false;
+        System.out.println("결제를 시작합니다: option=" + option + ", amount=" +amount);
+
+        Pay pay = findPay(option);
+        if (pay != null) {
+            result = pay.pay(amount);
+        }
+
+        if (result) {
+            System.out.println("결제가 성공했습니다.");
+            System.out.println();
+        } else {
+            System.out.println("결제가 실패했습니다.");
+            System.out.println();
+        }
+    }
+ public Pay findPay(String option){
+     if (option.equals("kakao")) {
+        return new KakaoPay();
+     } else if (option.equals("naver")) {
+         return new NaverPay();
+     } else {
+         System.out.println("결제 수단이 없습니다.");
+         return null;
+     }
+   }
+}
