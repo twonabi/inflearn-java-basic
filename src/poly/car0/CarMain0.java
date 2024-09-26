68d491ca806be8965e2242b1e014cf8c739879a1
jian <loveinansan1@gmail.com>
2024-09-26 11:19:51 +0900

    #81 김영한의 실전 자바 Basic
    class12_다형성3_역할과 구현 예제2
    
    K3를 운전하던 운전자가 Model3로 차량을 변경해서 운전해야 하는 상황!

--- src/poly/car0/CarMain0.java
+++ src/poly/car0/CarMain0.java
@@ -5,8 +5,14 @@ public class CarMain0 {
     public static void main(String[] args) {
         Driver driver = new Driver();
         K3Car k3Car = new K3Car();
-
         driver.setK3Car(k3Car);
         driver.drive();
+
+        // - K3를 운전하던 운전자가 Model3로 차량을 변경해서 운전하는 코드이다.
+        //추가
+        Model3Car model3Car = new Model3Car();
+        driver.setK3Car(null); // 기존 K3Car 의 참조를 제거!
+        driver.setModel3Car(model3Car); // 새로운 참조 추가!
+        driver.drive(); // 호출!!
     }
 }
